d0b25a0c4c3fde22bccf16adc8109c5268f1ce6a
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-13 21:06:09 +0800

    refactor: Add button and table to BookFunds page
    Add a button and table to the BookFunds page to allow users to add books and display book data. This enhances the functionality and user experience of the page.

--- app/bookFunds/page.tsx
+++ app/bookFunds/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const BookFunds = () => {
-  return <div className="flex justify-center items-center">BookFunds Page</div>;
+  const addBook = () => {
+    console.log("Add book");
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <button className="border p-2 mb-2"> + Add book</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Book 1</th>
+            <th>Book 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BookFunds;
