4289a1783868ee403c7ced1394f2b01c7af8e667
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-13 21:07:08 +0800

    refactor: Improve home page layout and add login and signup buttons
    Improve the layout of the home page by adding flexbox styling and centering the content vertically. Also, add login and signup buttons for better user navigation. This enhances the user experience and makes the home page more interactive.

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,25 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main>
+    <main className="flex flex-col justify-center items-center mt-20">
       <h1>Home</h1>
-      <button
-        type="button"
-        onClick={() => router.push("http://localhost:3000/auth/login")}
-        className="border-2 p-2"
-      >
-        Login
-      </button>
-      <button
-        type="button"
-        onClick={() => router.push("http://localhost:3000/auth/signup")}
-        className="border-2 p-2"
-      >
-        Signup
-      </button>
-      <p>Welcome to the home page.</p>
+      <div className="flex mt-2 justify-between">
+        <button
+          type="button"
+          onClick={() => router.push("http://localhost:3000/auth/login")}
+          className="border p-2 mx-2"
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          onClick={() => router.push("http://localhost:3000/auth/signup")}
+          className="border p-2 mx-2"
+        >
+          Signup
+        </button>
+      </div>
+      <p className="text-sm mt-2">Welcome to the home page.</p>
     </main>
   );
 }
