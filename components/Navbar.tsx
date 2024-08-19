bc7a82690ab8371b0251cf91e63853ec18b2c524
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-20 06:50:49 +0800

    refactor: Update Navbar component to use isActive function for active link styling
    Update the Navbar component to use the isActive function for determining the active link and applying the appropriate styling. This improves code clarity and ensures consistent styling for the active link in the navigation bar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,53 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+
   return (
     <nav className="flex pl-20 justify-evenly">
-      <Link href={"/requests"} className="">
+      <Link
+        href={"/requests"}
+        className={
+          isActive("/requests")
+            ? "text-black font-semibold dark:text-white dark:font-semibold"
+            : "text-base text-gray-600 dark:text-gray-300 font-normal"
+        }
+      >
         Requests
       </Link>
-      <Link href={"/quotations"} className="max-w-fit">
+      <Link
+        href={"/quotations"}
+        className={
+          isActive("/quotations")
+            ? "text-black font-semibold dark:text-white dark:font-semibold"
+            : "text-base text-gray-600 dark:text-gray-300 font-normal"
+        }
+      >
         Quotations
       </Link>
-      <Link href={"/reports"} className="max-w-fit">
+      <Link
+        href={"/reports"}
+        className={
+          isActive("/reports")
+            ? "text-black font-semibold dark:text-white dark:font-semibold"
+            : "text-base text-gray-600 dark:text-gray-300 font-normal"
+        }
+      >
         Report
       </Link>
-      <Link href={"/bookFunds"} className="max-w-fit">
+      <Link
+        href={"/bookFunds"}
+        className={
+          isActive("/bookFunds")
+            ? "text-black font-semibold dark:text-white dark:font-semibold"
+            : "text-base text-gray-600 dark:text-gray-300 font-normal"
+        }
+      >
         BookFunds
       </Link>
     </nav>
