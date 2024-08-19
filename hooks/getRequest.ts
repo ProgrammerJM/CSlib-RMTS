1d8670a3d7355b424dfef7abdd5c2833fc9cba2f
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-20 05:33:39 +0800

    refactor: Add getRequest hook for fetching book requests
    Add a new getRequest hook in the getRequest.ts file to handle fetching book requests from the server. This hook uses the useEffect and useState hooks to manage the state of the requests, error, and loading status. It makes a GET request to the "/api/books/request" endpoint and updates the state accordingly. This improves code organization and separates the fetching logic from the component that uses it.

---
+++ hooks/getRequest.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { BookRequest } from "@/interfaces/BookRequest";
+import React, { useEffect, useState } from "react";
+
+interface getRequestState {
+  requests: BookRequest[] | null;
+  error: string | null;
+  isLoading: boolean;
+}
+
+export default function GetRequest(): getRequestState {
+  const [requests, setBooks] = useState<BookRequest[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("http://localhost:5000/api/books/request");
+        if (!response.ok) {
+          throw new Error("Failed to Fetch Books");
+        }
+        const result: BookRequest[] = await response.json();
+        setBooks(result);
+      } catch (err: any) {
+        console.error(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+  return { requests, error, isLoading };
+}
